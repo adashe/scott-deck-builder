ae1483f71b88bf0e203b7d77b4022a1e4d296e08
Andrea Dashe <andrea.r.dashe@gmail.com>
2026-07-28 09:14:46 -0400

    Test redirects

--- lib/auth.py
+++ lib/auth.py
@@ -35,8 +35,8 @@ import base64
 
 PORTAL_LOGIN_URL = "https://www.siscc-portal.com/login"
 TEST_URL_1 = "https://www.youtube.com"
-TEST_URL_2 = "https://www.google.com"
-TEST_URL_3 = "https://www.nytimes.com"
+TEST_URL_2 = "https://www.nytimes.com"
+TEST_URL_3 = "https://www.google.com"
 
 # Name of this app's own session cookie (NOT the Supabase token).
 SESSION_COOKIE_NAME = "scott_deck_session"
